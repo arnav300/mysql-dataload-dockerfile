d7a91ea42f56571e98b100d0a542f90156932267
arnav300 <singh.arnav30@gmail.com>
2019-11-22 17:22:07 +0530

    Add files via upload

---
+++ CreateTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE titanic
+ ( 
+     survived boolean,
+     passengerClass int,
+     name varchar(100),
+     sex ENUM('male','female','other'),
+     age int,
+     siblingsOrSpousesAboard varchar(100),
+     parentsOrChildrenAboard varchar(100),
+     fare DECIMAL(10,3),
+     uuid varchar(100) NOT NULL 
+);
